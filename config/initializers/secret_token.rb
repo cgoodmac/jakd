efb429fd0657ed2039c5f2bbced9a468b91e2b3f
Chris Goodmacher <cgoodmac@gmail.com>
2012-12-01 10:58:46 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jakd::Application.config.secret_token = '71194c3104b472eefc800cfcb1ef93a9ec9bf3cd701016614e19f1056f4eea15afaf0a05718d80a2dc8e5bb174db70beda20cd87d08825b43e4543b4d3684610'
