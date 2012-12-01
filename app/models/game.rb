4cb78dac22e18ff8b0ddf990836c7cadd9e14127
Jasmine Chabra <jchabra@gmail.com>
2012-12-01 13:59:14 -0500

    user model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: games
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Game < ActiveRecord::Base
   has_and_belongs_to_many :users
 end
---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string(255)
+#  name            :string(255)
+#  password_digest :string(255)
+#  avatar          :string(255)
+#  balance         :decimal(, )      default(1000.0)
+#  games_won       :integer          default(0)
+#  games_lost      :integer          default(0)
+#  amount_won      :decimal(, )      default(0.0)
+#  amount_lost     :decimal(, )      default(0.0)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ActiveRecord::Base
+  has_and_belongs_to_many :games
+end
