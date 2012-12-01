9c1ff50a7253cb11373eb6f9e17f1d56ff20e805
Jasmine Chabra <jchabra@gmail.com>
2012-12-01 17:14:51 -0500

    javascript file

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -12,16 +12,11 @@ function hide_button()
   $('#bet').show();
 }
 
+function go()
+{
+  $('#go').val();
+  $('#bet').hide();
+  
+}
 
 
-// <%= form_tag '/games', :remote => true do %>
-//   <div id= "ready">
-//     <%= submit_tag 'Ready to Play' %>
-//   </div><!--#start-->
-//   <div id= "bet">
-//     How much do you want to bet?    <input type="number" placeholder="Insert $ amount" />
-//     <%= submit_tag 'Go', :id => 'go' %>
-//   </div><!--#bet-->
-
-//   <div id="bet"></div>
-// <% end %>
